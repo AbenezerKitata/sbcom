5cca7e085bc79820002dfc538adf996fef5203dc
AbenezerK <Abi4kit@gmail.com>
2024-04-14 16:05:24 -0700

    testing if env works

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
+  console.log(process.env.NEXTAUTH_URL);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Colors are a reflection of our ...
+      {process.env.NEXTAUTH_URL} &nbsp; Testing my env works...
     </main>
   );
 }
