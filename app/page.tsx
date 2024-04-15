18a76534b785da026e328a32bdc0f730eecc03f5
AbenezerK <Abi4kit@gmail.com>
2024-04-15 10:45:08 -0700

    testing if nginx modification changed my site

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Testing my env works...
+      Second test for github actions integration
     </main>
   );
 }
