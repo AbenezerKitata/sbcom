d003ee51ad03b398e12312a1f4996c04e08d80d1
AbenezerK <Abi4kit@gmail.com>
2024-04-15 07:51:22 -0700

    delete the .env.production thing that is not needed. Saving env files in my .env and gh secrets

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
-  console.log(process.env.NEXTAUTH_URL);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {process.env.NEXTAUTH_URL} &nbsp; Testing my env works...
+      Testing my env works...
     </main>
   );
 }
