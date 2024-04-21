6f6a2ca355b02d2445f1a3ed784ab122f86d81f5
AbenezerK <Abi4kit@gmail.com>
2024-04-21 11:03:14 -0700

    added tailwind custom applies for typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Fifth test for github actions integration !!
+    <main className="flex min-h-screen flex-col items-center gap-10 p-24">
+      <h1>Some Heading For Testing Bigger Fonts</h1>
+      <h2>Smaller Heading For Testing Slightly Lower Headings </h2>
+      <p className="p">
+        Testing a paragraph: Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Modi, at ipsam! Vitae similique dolorum rem, ducimus reiciendis
+        fugit quod sint dolor aliquam voluptatem alias mollitia exercitationem
+        voluptates velit nostrum corrupti Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Inventore perferendis quibusdam laborum
+        cumque laboriosam doloremque suscipit alias quo saepe aliquam. Natus
+        delectus exercitationem veniam doloremque impedit ut placeat repellat,
+        necessitatibus nihil earum iste consectetur alias ducimus ullam
+        assumenda deleniti tempora quis aliquam cum praesentium voluptates.
+        Dolor et nesciunt est aliquid?
+      </p>
     </main>
   );
 }
