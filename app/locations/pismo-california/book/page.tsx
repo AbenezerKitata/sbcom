7379ec02d864088471bd2c42339142abf6d21e71
AbenezerK <Abi4kit@gmail.com>
2024-04-25 11:29:50 -0700

    added pismo booking page

---
+++ app/locations/pismo-california/book/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookPismo = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center py-10 px-24">
+      <h1>Step 1 of 5</h1>
+    </div>
+  );
+};
+
+export default BookPismo;
